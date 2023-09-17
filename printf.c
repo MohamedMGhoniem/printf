627b80cab4d1401c86b930329fad5da9f783b02f
Ema-Saad <Eman@example.com>
2023-09-17 10:45:40 -0700

    agian

--- printf.c
+++ printf.c
@@ -1,11 +1,10 @@
 /**
-* printf - function to print
-* @format: string
-* @...: indefinte var
-*
-*Return: num of chars
-*/
-
+ * _printf - function to print formatted output
+ * @format: string
+ * @...: indefinite arguments
+ *
+ * Return: number of characters printed
+ */
 #include "main.h"
 int _printf(const char *format, ...)
 {
@@ -15,9 +14,14 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
-	for (; *format; format++)
+	while (*format)
 	{
-		if (*format == '%')
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			ch_count++;
+		}
+		else
 		{
 			format++;
 			if (*format == '\0')
@@ -29,7 +33,8 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == 'c')
 			{
-				char c = (char)va_arg(args, int);
+				char c = va_arg(args, int);
+
 				write(1, &c, 1);
 				ch_count++;
 			}
@@ -37,17 +42,13 @@ int _printf(const char *format, ...)
 			{
 				char *str = va_arg(args, char *);
 				int s_len = strlen(str);
+
 				write(1, str, s_len);
 				ch_count += s_len;
 			}
 		}
-		else
-		{
-			write(1, format, 1);
-			ch_count++;
-		}
+		format++;
 	}
-
 	va_end(args);
 	return (ch_count);
 }
